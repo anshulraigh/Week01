757d3b5122707f02ef2e21bd5705ad2a9efc8f84
Anshul Rai <anshulrai02092003@gmail.com>
2025-01-09 21:00:22 +0530

    Add program to convert kilometers into miles

---
+++ Level1/KmToMiles.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class KmToMiles{
+	public static void main(String[] args){
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter distance in Kilometer : ");
+	double km = input.nextDouble();
+	
+	// Converting km to miles
+	double distanceInMiles = km / 1.6;
+	
+	System.out.println("The total miles is " + distanceInMiles + " mile for the given " + km + " km");
+	
+	input.close();
+	}
+	
+}
